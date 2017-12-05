6a38e018026e3e27bcbf9979c772500ebd0bdaaa
Yuiri Arakaki <e175719@ie.u-ryukyu.ac.jp>
2017-12-05 10:05:54 +0900

    first commit.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import jp.ac.uryukyu.ie.ex3_2nd.pair31.*;
+
+public class Main {
+
+    public static void main(String[] args){
+    Dice dice = new Dice();
+    dice.play();
+    System.out.println(dice.getValue());
+
+    }
+}
