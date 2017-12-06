3665a7f431f00d6c7d6a803d6d348c30d4327e54
Yuiri Arakaki <e175719@ie.u-ryukyu.ac.jp>
2017-12-06 10:47:44 +0900

    ex3_3

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,15 @@ import jp.ac.uryukyu.ie.ex3_2nd.pair31.*;
 public class Main {
 
     public static void main(String[] args){
-    Dice dice = new Dice();
-    dice.play();
-    System.out.println(dice.getValue());
+	Dice dice = new Dice();
+	dice.play();
+	System.out.println(dice.getValue());
 
+        ExDice exdice = new ExDice(5,7);
+        for(int i = 0; i < 10; i++){
+            exdice.play();
+            System.out.println("exdice" + i + "回目:"+exdice.getValue());
+	}
+	
     }
 }
